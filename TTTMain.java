656c00ecf3e81f648347fb2adf8180bcf1030b1a
Sam Clarke <SClarke4772@student.whatcom.edu>
2024-03-09 17:24:50 -0800

    cleaned up code and added everything but computer play

--- TTTMain.java
+++ TTTMain.java
@@ -1,9 +1,66 @@
+import java.util.*;
+
 public class TTTMain
 {
     public static void main(String[] args)
     {
-        TTTManager game = new TTTManager(5);
+        Scanner input = new Scanner(System.in);
+        Scanner inputInt = new Scanner(System.in);
+
+        boolean hasQuit = false;
+
+        intro();
+
+        while (!hasQuit)
+        {
+            switch(menu(input))
+            {
+                case 'p':
+                    playGame(input, inputInt);
+                    break;
+                case 's':
+                    break;
+                case 'r':
+                    break;
+                case 'q':
+                    hasQuit = true;
+                    break;
+            }  
+        }
+    }
+
+    public static void playGame(Scanner input, Scanner inputInt)
+    {
+        System.out.print("\nWhat board size do you want to play with? Enter 3 for (3x3), 4 for (4x4), etc: ");
+        int boardSize = inputInt.nextInt();
+        System.out.print("Would you like to play with X's or O's? ");
+        TTTManager game = new TTTManager(boardSize, input.nextLine().toUpperCase().charAt(0));
 
         game.printBoard();
+        while (game.winner() == null)
+        {
+            game.oneRound(input);
+        }
+
+        System.out.println(game.winner() + " won the game!");
+    }
+
+    public static void intro()
+    {
+        System.out.printf("%s%n%s%n%s%n",
+        "Welcome to Sam's Tic Tac Toe game!",
+        "This program will allow you play Tic Tac Toe games against the computer.", 
+        "One thing to note: the Tic Tac Toe board's (0,0) origin is in the bottom left of the board.");
+    }
+
+    public static char menu(Scanner input)
+    {
+        System.out.printf("%n%s%n%s%n%s%n%s%n%s%n",
+        "Please choose an action:",
+        "(p) Play one game of Tic Tac Toe",
+        "(s) Save the previous game to a file",
+        "(r) Show the overall results",
+        "(q) Quit the program");
+        return input.nextLine().toLowerCase().charAt(0);
     }
 }
--- TTTManager.java
+++ TTTManager.java
@@ -2,32 +2,153 @@ import java.util.*;
 
 public class TTTManager
 {  
+    private static final char EMPTY_CHAR = '\u0000';
     private char[][] gameBoard;
+    private int[][] winCombinations;
+    private int[] playerArray;
+    private int[] computerArray;
 
-    public TTTManager(int boardSize)
+    private int boardSize;
+    private char playerLetter;
+
+    public TTTManager(int boardSize, char playerLetter)
     {
         gameBoard = new char[boardSize][boardSize];
-        for (int i = 0; i < boardSize; i++)
-        {
-            for (int j = 0; j < boardSize; j++)
-            {
-                gameBoard[i][j] = ' ';
-            }
-        }
+        winCombinations = new int[boardSize*2+2][boardSize*boardSize];
+        playerArray = new int[boardSize*boardSize];
+        computerArray = new int[boardSize*boardSize];
+        this.boardSize = boardSize;
+        this.playerLetter = playerLetter;
+
+        generateWinCombinations();
     }
 
+    private void generateWinCombinations()
+    {
+        for (int i = 0; i < boardSize; i++)
+        {
+            int[] horzCombination = new int[boardSize*boardSize];
+            for (int j = 0; j < boardSize; j++)
+            {
+                horzCombination[i * boardSize + j] = 1;
+            }
+            winCombinations[i] = horzCombination;
+        }
+
+        for (int i = 0; i < boardSize; i++)
+        {
+            int[] vertCombination = new int[boardSize*boardSize];
+            for (int j = 0; j < boardSize; j++)
+            {
+                vertCombination[i + j * boardSize] = 1;
+            }
+            winCombinations[i + boardSize] = vertCombination;
+        }
+
+        int[] rightDiagonal = new int[boardSize*boardSize];
+        int[] leftDiagonal = new int[boardSize*boardSize];
+        for (int i = 0; i < boardSize; i++)
+        {
+            rightDiagonal[i * (boardSize + 1)] = 1;
+            leftDiagonal[(i + 1) * (boardSize - 1)] = 1;
+        }
+        winCombinations[boardSize*2] = rightDiagonal;
+        winCombinations[boardSize*2+1] = leftDiagonal;
+    }
 
     public void printBoard()
     {
-        for (int i = 0; i < gameBoard.length; i++)
+        for (int i = 0; i < boardSize; i++)
         {
-            System.out.println(String.join("", Collections.nCopies(gameBoard.length, "+---")) + "+");
-            for (int j = 0; j < gameBoard.length; j++)
+            System.out.println(String.join("", Collections.nCopies(boardSize, "+---")) + "+");
+            for (int j = 0; j < boardSize; j++)
             {
-                System.out.print("| " + gameBoard[i][j] + " ");
+                if (gameBoard[i][j] == EMPTY_CHAR)
+                {
+                    System.out.print("|   ");
+                }
+                else
+                {
+                    System.out.print("| " + gameBoard[i][j] + " ");
+                }
             }
             System.out.println("|");
         }
-        System.out.print(String.join("", Collections.nCopies(gameBoard.length, "+---")) + "+");
+        System.out.println(String.join("", Collections.nCopies(boardSize, "+---")) + "+");
+    }
+
+    public void oneRound(Scanner input)
+    {
+        System.out.print("Where would you like to play?\nEnter coordinates in the form x,y: ");
+        String[] coords = input.nextLine().split(",");
+        int xCoord = Integer.parseInt(coords[0]);
+        int yCoord = Integer.parseInt(coords[1]);
+        while (xCoord < 0 || xCoord > boardSize-1 || yCoord < 0 || yCoord > boardSize-1 || gameBoard[boardSize-1-yCoord][xCoord] != EMPTY_CHAR)
+        {
+            System.out.print("The coordinates (" + coords[0] + "," + coords[1] + ") are either out of range or already filled. Please try again: ");
+            coords = input.nextLine().split(",");
+            xCoord = Integer.parseInt(coords[0]);
+            yCoord = Integer.parseInt(coords[1]);
+        }
+
+        playerArray[(boardSize-1-yCoord)*boardSize+xCoord] = 1;
+        gameBoard[boardSize-1-yCoord][xCoord] = playerLetter;
+        printBoard();
+    }
+
+    private boolean fullBoard()
+    {
+        for (int i = 0; i < gameBoard.length; i++)
+        {
+            for (int j = 0; j < gameBoard[0].length; j++)
+            {
+                if (gameBoard[i][j] == EMPTY_CHAR)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public String winner()
+    {
+        boolean playerWin;
+        boolean computerWin;
+
+        if (fullBoard())
+        {
+            return "The Cat";
+        }
+
+        for (int[] combination : winCombinations)
+        {
+            playerWin = true;
+            computerWin = true;
+            for (int i = 0; i < combination.length; i++)
+            {
+                if (combination[i] == 1 && playerArray[i] != 1)
+                {
+                    playerWin = false;
+                }
+                if (combination[i] == 1 && computerArray[i] != 1)
+                {
+                    computerWin = false;
+                }
+                if (!playerWin && !computerWin)
+                {
+                    break;
+                }
+            }
+            if (playerWin)
+            {
+                return "You";
+            }
+            else if (computerWin)
+            {
+                return "The Computer";
+            }
+        }
+        return null;
     }
 }
