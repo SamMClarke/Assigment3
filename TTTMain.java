4733bb1c6dfe6f2b637a51ab746032e393e51ae0
Sam Clarke <SClarke4772@student.whatcom.edu>
2024-03-11 11:38:40 -0700

    added comments

--- TTTMain.java
+++ TTTMain.java
@@ -1,3 +1,12 @@
+/** 
+ * TTTMain.java
+ * @Author: Sam Clarke
+ * @Date: 3-11-2024
+ * @Class: CS&145
+ * @Assignment: Assignment #3
+ * @Purpose: Create an interactive Tic Tac Toe game
+ */
+
 import java.util.*;
 import java.io.*;
 
@@ -5,11 +14,11 @@ public class TTTMain
 {
     public static void main(String[] args)
     {
+        //OBJECTS AND VARIABLES
         Scanner input = new Scanner(System.in);
         Scanner inputInt = new Scanner(System.in);
         TTTManager currentGame = null;
         int[] results = new int[3];
-
         boolean hasQuit = false;
 
         intro();
@@ -18,10 +27,10 @@ public class TTTMain
         {
             switch(menu(input))
             {
-                case 'p':
+                case 'p': //Play one game with the user
                     currentGame = playGame(input, inputInt, results);
                     break;
-                case 's':
+                case 's': //Save the previous game to a file
                     if (currentGame != null)
                     {
                         saveGame(input, currentGame);
@@ -31,16 +40,22 @@ public class TTTMain
                         System.out.println("No game to save, please play a game first!");
                     }
                     break;
-                case 'r':
+                case 'r': //Show the overall results
                     showResults(results);
                     break;
-                case 'q':
+                case 'q': //Quit the program
                     hasQuit = true;
                     break;
             }  
         }
     }
 
+    /**
+     * Saves the previous game to a File.
+     * 
+     * @param input Scanner object for gathering file name
+     * @param game Previous game object
+     */
     public static void saveGame(Scanner input, TTTManager game)
     {
         try
@@ -57,6 +72,11 @@ public class TTTMain
         }
     }
 
+    /**
+     * Prints out the overall results to the console.
+     * 
+     * @param results Array that stores the overall results data
+     */
     public static void showResults(int[] results)
     {
         System.out.println("\nYour Wins: " + results[0]);
@@ -64,8 +84,17 @@ public class TTTMain
         System.out.println("Cat (tie) Wins: " + results[2]);
     }
 
+    /**
+     * Plays one game of Tic Tac Toe with the user
+     * 
+     * @param input Scanner object for gathering user string input
+     * @param inputInt Scanner object for gathering user int inputs
+     * @param results Array taht stores the overall results data
+     * @return The game object
+     */
     public static TTTManager playGame(Scanner input, Scanner inputInt, int[] results)
     {
+        //GATHER USER INPUT DATA
         System.out.print("\nWhat board size do you want to play with? Enter 3 for (3x3), 4 for (4x4), etc: ");
         int boardSize = inputInt.nextInt();
         System.out.print("Would you like to play with X's or O's? ");
@@ -75,19 +104,27 @@ public class TTTMain
             System.out.print("Invalid letter. Please choose either X or O: ");
             playerLetter = input.nextLine().toUpperCase().charAt(0);
         }
-        TTTManager game = new TTTManager(boardSize, playerLetter);
 
+        //CREATE GAME OBJECT
+        TTTManager game = new TTTManager(boardSize, playerLetter);
         game.printBoard(System.out);
-        while (game.winner() < 0)
+
+        while (game.winner() < 0) //KEEP TAKING TURNS UNTIL WINNER
         {
             game.oneRound(input);
         }
-        results[game.winner()]++;
+        results[game.winner()]++; //UPDATE RESULTS
         System.out.println(winnerName(game.winner()) + " won the game!");
 
         return game;
     }
 
+    /**
+     * Gets the name of the winner.
+     * 
+     * @param winner Integer value that represents specific winner (0 = user, 1 = computer, 2 = tie)
+     * @return A string of the winner's name
+     */
     public static String winnerName(int winner)
     {
         if (winner == 0) {
@@ -100,6 +137,9 @@ public class TTTMain
         return null;
     }
 
+    /**
+     * Prints an intro to the game.
+     */
     public static void intro()
     {
         System.out.printf("%s%n%s%n%s%n",
@@ -108,6 +148,12 @@ public class TTTMain
         "One thing to note: the Tic Tac Toe board's (0,0) origin is in the bottom left of the board.");
     }
 
+    /**
+     * Prints the menu to the game.
+     * 
+     * @param input Scanner object for gathering user input
+     * @return The user's character input
+     */
     public static char menu(Scanner input)
     {
         System.out.printf("%n%s%n%s%n%s%n%s%n%s%n",
--- TTTManager.java
+++ TTTManager.java
@@ -1,9 +1,20 @@
+/** 
+ * TTTManager.java
+ * @Author: Sam Clarke
+ * @Date: 3-11-2024
+ * @Class: CS&145
+ * @Assignment: Assignment #3
+ * @Purpose: Create an interactive Tic Tac Toe game
+ */
+
 import java.util.*;
 import java.io.*;
 
 public class TTTManager
 {  
+    //CLASS OBJECTS, VARIABLES, AND CONSTANTS
     private static final char EMPTY_CHAR = '\u0000';
+
     private char[][] gameBoard;
     private int[][] winCombinations;
     private int[] playerArray;
@@ -13,12 +24,20 @@ public class TTTManager
     private char playerLetter;
     private char computerLetter; 
 
+    /**
+     * Constructs a game object.
+     * 
+     * @param boardSize Width (and height) of the game board
+     * @param playerLetter The letter (X or O) the player will be controlling
+     */
     public TTTManager(int boardSize, char playerLetter)
     {
+        //CREATE GAME ARRAYS
         gameBoard = new char[boardSize][boardSize];
         winCombinations = new int[boardSize*2+2][boardSize*boardSize];
         playerArray = new int[boardSize*boardSize];
         computerArray = new int[boardSize*boardSize];
+
         this.boardSize = boardSize;
         this.playerLetter = playerLetter;
         if (playerLetter == 'X')
@@ -33,8 +52,16 @@ public class TTTManager
         generateWinCombinations();
     }
 
+    /**
+     * Generates all the possible win combinations for the game board.
+     */
     private void generateWinCombinations()
     {
+        //GENERATE HORIZONTAL COMBINATIONS:
+        //Example 3x3:
+        //111000000
+        //000111000
+        //000000111
         for (int i = 0; i < boardSize; i++)
         {
             int[] horzCombination = new int[boardSize*boardSize];
@@ -45,6 +72,11 @@ public class TTTManager
             winCombinations[i] = horzCombination;
         }
 
+        //GENERATE VERTICAL COMBINATIONS
+        //Example 3x3:
+        //100100100
+        //010010010
+        //001001001
         for (int i = 0; i < boardSize; i++)
         {
             int[] vertCombination = new int[boardSize*boardSize];
@@ -55,6 +87,10 @@ public class TTTManager
             winCombinations[i + boardSize] = vertCombination;
         }
 
+        //GNEREATE DIAGONAL COMBINATIONS
+        //Example 3x3:
+        //100010001
+        //001010100
         int[] rightDiagonal = new int[boardSize*boardSize];
         int[] leftDiagonal = new int[boardSize*boardSize];
         for (int i = 0; i < boardSize; i++)
@@ -66,54 +102,75 @@ public class TTTManager
         winCombinations[boardSize*2+1] = leftDiagonal;
     }
 
+    /**
+     * Prints the current game board.
+     * 
+     * @param output The place to print the game board to
+     */
     public void printBoard(PrintStream output)
     {
         for (int i = 0; i < boardSize; i++)
         {
-            output.println(String.join("", Collections.nCopies(boardSize, "+---")) + "+");
+            output.println(String.join("", Collections.nCopies(boardSize, "+---")) + "+"); //Java 8 String.repeat workaround
             for (int j = 0; j < boardSize; j++)
             {
-                if (gameBoard[i][j] == EMPTY_CHAR)
+                if (gameBoard[i][j] == EMPTY_CHAR) //EMPTY CELL
                 {
                     output.print("|   ");
                 }
-                else
+                else //FILLED CELL
                 {
                     output.print("| " + gameBoard[i][j] + " ");
                 }
             }
             output.println("|");
         }
-        output.println(String.join("", Collections.nCopies(boardSize, "+---")) + "+");
+        output.println(String.join("", Collections.nCopies(boardSize, "+---")) + "+"); //Java 8 String.repeat workaround
     }
 
+    /**
+     * Completes one round (one X played and one O played) of the game.
+     * 
+     * @param input Scanner object for gathering user coordinates
+     */
     public void oneRound(Scanner input)
     {
         System.out.print("Where would you like to play?\nEnter coordinates in the form x,y: ");
-        String[] coords = input.nextLine().split(",");
+        String[] coords = input.nextLine().split("[/|.|,| |]"); //SPLIT COORIDATES INTO X,Y COMPONENTS
         int xCoord = Integer.parseInt(coords[0]);
         int yCoord = boardSize-1-Integer.parseInt(coords[1]);
+        //WHILE INVALID COORDINATES, ASK AGAIN
         while (xCoord < 0 || xCoord > boardSize-1 || yCoord < 0 || yCoord > boardSize-1 || gameBoard[yCoord][xCoord] != EMPTY_CHAR)
         {
             System.out.print("The coordinates (" + coords[0] + "," + coords[1] + ") are either out of range or already filled. Please try again: ");
-            coords = input.nextLine().split(",");
+            coords = input.nextLine().split("[/|.|,| |]");
             xCoord = Integer.parseInt(coords[0]);
             yCoord = boardSize-1-Integer.parseInt(coords[1]);
         }
 
+        //UPDATE PLAYER ARRAY AND GAME BOARD
         playerArray[(yCoord)*boardSize+xCoord] = 1;
         gameBoard[yCoord][xCoord] = playerLetter;
         printBoard(System.out);
-        if (!checkWinner(playerArray))
+
+        if (!checkWinner(playerArray)) //IF THE PLAYER HASN'T WON YET
         {
+            //COMPUTER'S TURN
             System.out.println("\nMy turn...\n");
             computerMove();
             printBoard(System.out);
         }
     }
 
+    /**
+     * Places computer letter in a cell using three rules:
+     * -If computer is one move away from winning -> win
+     * -If the user is one move away from winning -> block
+     * -Else play a letter in a random cell.
+     */
     private void computerMove()
     {
+        //CHECK FOR COMPUTER WINS
         for (int i = 0; i < gameBoard.length; i++)
         {
             for (int j = 0; j < gameBoard[i].length; j++)
@@ -128,12 +185,14 @@ public class TTTManager
                     }
                     else
                     {
+                        //UNDO MOVE IF IT DOES NOT LEAD TO A WIN
                         computerArray[(i*boardSize)+j] = 0;
                     }
                 }
             }
         }
 
+        //CHECK FOR USER BLOCKS
         for (int i = 0; i < gameBoard.length; i++)
         {
             for (int j = 0; j < gameBoard[i].length; j++)
@@ -150,12 +209,14 @@ public class TTTManager
                     }
                     else
                     {
+                        //UNDO MOVE IF IT DOES NOT LEAD TO A BLOCK
                         playerArray[(i*boardSize)+j] = 0;
                     }
                 }
             }
         }
 
+        //PLACE LETTER IN RANDOM CELL
         Random r = new Random();
         int row;
         int col;
@@ -169,6 +230,12 @@ public class TTTManager
         gameBoard[row][col] = computerLetter;
     }
 
+    /**
+     * Checks if the given player has won the game.
+     * 
+     * @param array The array of the player to check for
+     * @return A booleon of whether or not they won
+     */
     private boolean checkWinner(int[] array)
     {
         boolean hasWon = false;
@@ -191,6 +258,11 @@ public class TTTManager
         return hasWon;
     }
 
+    /**
+     * Checks if the board is full and therefore a tie.
+     * 
+     * @return A boolean of whether or not the board is full
+     */
     private boolean fullBoard()
     {
         for (int i = 0; i < gameBoard.length; i++)
@@ -206,20 +278,25 @@ public class TTTManager
         return true;
     }
 
+    /**
+     * Gets the winner as an int value (to be used in the results array later).
+     * 
+     * @return The int value of the winner
+     */
     public int winner()
     {
-        if (checkWinner(playerArray))
+        if (checkWinner(playerArray)) //PLAYER WINS
         {
             return 0;
         }
-        else if (checkWinner(computerArray))
+        else if (checkWinner(computerArray)) //COMPUTER WINS
         {
             return 1;
         }
-        else if (fullBoard())
+        else if (fullBoard()) //TIE
         {
             return 2;
         }
-        return -1;
+        return -1; //NO WINNER
     }
 }
